4385f485141cc0d2c0c30b13b8059d7c876a3cec
AlexandreHurbe <37171772+AlexandreHurbe@users.noreply.github.com>
2018-11-17 22:23:45 +1100

    Added basic player controls that are still being worked on

--- Assets/WorldWarToon/Script/Player/PlayerCamera.cs
+++ Assets/WorldWarToon/Script/Player/PlayerCamera.cs
@@ -3,29 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour {
-    public Transform target;
+    public GameObject target;
+    public float panSpeed = 20f;
+    public float panBorderThickness = 10f;
+    public Vector2 panLimit;
+    public float rotationSpeed = 50f;
+    public float scrollSpeed = 2f;
     public float smoothing = 5f;
     public float rotationSmoothing;
+
     private Vector3 offset;
+    private float cameraDistance;
+    private float cameraDistanceMin = 5f;
+    private float cameraDistanceMax = 18f;
+    Vector3 rotationMask = new Vector3(0, 1, 0);
 
     private void Start() {
-        this.transform.position = new Vector3(target.position.x, target.position.y + 12, target.position.z-10);
-        offset = target.position - transform.position;
+        offset = new Vector3(target.transform.position.x, target.transform.position.y + 18, target.transform.position.z - 6);
+        this.transform.position = offset;
+        cameraDistance = this.transform.position.y;
+    }
+
+    
+    private void Update() {
+        cameraDistance -= Input.GetAxis("Mouse ScrollWheel") * scrollSpeed;
+        cameraDistance = Mathf.Clamp(cameraDistance, cameraDistanceMin, cameraDistanceMax);
+        this.transform.position = new Vector3(this.transform.position.x, cameraDistance, this.transform.position.z);
+        
+        Vector3 pos = transform.position;
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        if (Input.GetMouseButton(2) && mouseX != 0) {
+            transform.RotateAround(target.transform.position, rotationMask, rotationSpeed * mouseX * Time.deltaTime);
+            return;
+        }
+
+        pos.z += mouseY * Time.deltaTime;
+        pos.x += mouseX * Time.deltaTime;
+
+        //pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
+        //pos.z = Mathf.Clamp(pos.z, -panLimit.y, panLimit.y);
+
+        transform.position = pos;
+        
+        if (Input.GetKeyDown(KeyCode.F)) {
+            this.transform.position = offset;
+        }
     }
 
     /*
-    private void FixedUpdate() {
-        Vector3 targetCamPos = target.position + offset;
-        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
-        Quaternion reformedTargetRotation = target.rotation;
-        //reformedTargetRotation.z = 0;
-        //reformedTargetRotation.y = target.rotation.y;
-        //reformedTargetRotation.x = 55;
-        transform.rotation = reformedTargetRotation;
-        //reformedTargetRotation.y = transform.rotation.y;
-        //transform.rotation = Quaternion.Lerp(transform.rotation, reformedTargetRotation, Time.deltaTime);
-    }*/
-
     private void LateUpdate() {
         float currentAngle = transform.eulerAngles.y;
         float desiredAngle = target.transform.eulerAngles.y;
@@ -36,5 +63,7 @@ public class PlayerCamera : MonoBehaviour {
 
         transform.LookAt(target.transform);
 
-    }
+    }*/
+
+    
 }
---
+++ Assets/WorldWarToon/Script/Player/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    Rigidbody playerRigidBody;
+
+
+    private void Awake() {
+        playerRigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Debug.Log("Horizontal: " + h);
+        Debug.Log("Vertical: " + v);
+        Move(h, v);
+    }
+
+    private void Move(float h, float v) {
+        Vector3 movement = new Vector3 (0, 0, 0);
+        
+        if (v > 0) {
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, Camera.main.transform.localEulerAngles.y, transform.localEulerAngles.z);
+        }
+        movement.Set(h, 0, v);
+        playerRigidBody.MovePosition(transform.position + (movement*Time.deltaTime*3f));
+    }
+}
